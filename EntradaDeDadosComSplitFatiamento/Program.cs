d80532ec031bba8b956a7e2d1883f6a4bbe74c28
Welerson Silva <welerson.cote2@gmail.com>
2023-09-22 22:26:41 -0300

    Adicionar arquivos de projeto.

---
+++ EntradaDeDadosComSplitFatiamento/Program.cs
@@ -0,0 +1,22 @@
+﻿string frase = Console.ReadLine();
+string x = Console.ReadLine();
+string y = Console.ReadLine();
+string z = Console.ReadLine();
+
+
+string[] v = Console.ReadLine().Split(' ');
+//Espaço em branco determina aonde eu quero que haja a separação de palavras (recortando os pedaços) em seguida ele armazena no vetor. Lembrando a ideia de Arrays e armazenando da posição 0 em diante.
+string a = v[0];
+string b = v[1];
+string c = v[2];
+Console.WriteLine(v);
+
+Console.WriteLine("Você digitou: ");
+
+Console.WriteLine(frase);
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
